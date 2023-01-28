3df561027e7d719d91608ef3bdcfe6d7add2eb4a
Lultime <117547665+Lultime@users.noreply.github.com>
2023-01-27 21:45:44 -0500

    Add files via upload

---
+++ dev java/SimulationBoucle2.java
@@ -0,0 +1,12 @@
+public class SimulationBoucle2{
+	public static void main(String ars[]){
+		for(int i=0;i<10;i++){
+			if(i==5){
+				System.out.println("M pap fe wout la anko");
+				continue;
+			}
+			System.out.println(i);
+			//0 1 2 3 4 5 6 7 8 9 10
+		}
+	}
+}
