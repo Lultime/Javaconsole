0fc3634596f48d0d4deab8ff08b041734b5c6eda
Lultime <117547665+Lultime@users.noreply.github.com>
2023-01-28 21:42:48 -0500

    Add files via upload

---
+++ dev java/SimulationBoucle.java
@@ -0,0 +1,13 @@
+public class SimulationBoucle{
+	public static void main(String ars[]){
+		for(int i=0;i<20;i++){
+
+			if(i==10){
+				System.out.println("Arret de boucle ici");
+				break;
+			}
+			System.out.println(i);
+			//0 1 2 3 4 5 6 7 8 9 10
+		}
+	}
+}
